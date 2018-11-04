c9ffc8910fe3109afd911121e6472f9922c8d174
gannonprudhomme <gannonprudhomme@gmail.com>
2018-11-03 21:14:59 -0500

    Removed old tablet and mobile folders, moved handlebars as new main page

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ var fs = require('fs')
 var queryString = require('querystring')
 var request = require('request')
 
-var exHbs = require('express-handlebars') //
+// Handlebars stuff
+var exHbs = require('express-handlebars') 
 var helpers = require('handlebars-helpers')()
 
+// Initialize Express-Handlebars
 var handleBars = exHbs.create({
   extname: 'hbs',
   defaultLayout: 'layout',
-  layoutsDir: __dirname + '/public/handlebars/views/layouts/',
-  partialsDir: __dirname + '/public/handlebars/views/partials',
+  layoutsDir: __dirname + '/public/views/layouts/',
+  partialsDir: __dirname + '/public/views/partials',
   helpers: helpers
 })
 
@@ -25,22 +27,23 @@ var http = require('http')
 var server = http.createServer(app)
 var io = require('socket.io').listen(server)
 
+var authenticated = false;
+
 // Routes
 var spotify = require('./routes/spotify.js')
 var discord = require('./routes/discord.js')
 var desktop = require('./routes/desktop.js')
 var socket = require('./routes/sockets.js')(io)
 
+// Attach Handlebars
 // 'hps' is the internal name of handleBars and the extension(.hbs) name
 // 'layout' is the default layout(layout.hbs) to be used by HandleBars
 app.engine('hbs', handleBars.engine);
-app.set('views', path.join(__dirname + '/public/handlebars/views'));
+app.set('views', path.join(__dirname + '/public/views'));
 app.set('view engine', 'hbs') // 'hbs' is connected to the app.engine('hbs', ...)
 
 app.use(discord)
 app.use(socket)
-//app.use(desktop.router(io))
-//app.use(spotify.router(io))
 
 // Set properties
 app.use(express.static(__dirname))
@@ -54,66 +57,8 @@ app.use(function(req, res, next) {
   next();
 })
 
-var authenticated = false;
 
-// app.use(express.static(__dirname + '/public/'))
-
-// Redirect to spotify authentication?
 app.get('/tablet', (req, res) => {
-  if(!authenticated) {
-    var state = generateRandomString(8);
-
-    //res.cookie(stateKey, state);
-    var scope = 'user-read-private user-read-playback-state user-modify-playback-state user-read-currently-playing'
-    res.redirect('https://accounts.spotify.com/authorize?' +
-      queryString.stringify({
-        response_type:'code',
-        client_id: spotify.client_id,
-        scope: scope,
-        redirect_uri: spotify.redirect_uri,
-        state: state
-      })
-    )
-
-    authenticated = true;
-  } else {
-    res.sendFile(path.join(__dirname + '/public/tablet/index.html'))
-
-    // After authenticating, get the access and refresh tokens
-    // have a function that takes req as a parameter(or req.query)
-    var code = req.query.code || null;
-    var state = req.query.state || null;
-
-    access_code = code;
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: spotify.redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(spotify.client_id + ':' + spotify.client_secret).toString('base64'))
-      },
-      json: true
-    }
-
-    request.post(authOptions, function(error, response, body) {
-      if(!error && response.statusCode === 200) {
-        spotify.setTokens(body.access_token, body.refresh_token)
-      } else {
-        console.log(error)
-      }
-    })
-  }
-})
-
-app.get('/mobile', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/mobile/control.html'))
-})
-
-app.get('/handlebars', (req, res) => {
-  //  Load in settings file
   if(!authenticated) {
     var state = generateRandomString(8);
 
@@ -180,16 +125,12 @@ server.listen(port, (err) => {
   desktop.loadVolumeData()
 })
 
-function maximizeWindow(programName) {
-
-}
-
 var generateRandomString = function(length) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  };
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
