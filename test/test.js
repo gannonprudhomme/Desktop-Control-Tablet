c942dd18e9b2d9e11cbbc78f9823d1e344d8ba41
gannonprudhomme <gannonprudhomme@gmail.com>
2018-11-25 22:35:41 -0600

    Added basic server testing

--- test/test.js
+++ test/test.js
@@ -2,11 +2,14 @@ var expect = require('chai').expect
 var commands = require('../routes/commands.js')
 var fs = require('fs')
 var desktop = require('../routes/desktop.js')
+var io = require('socket.io-client')
 
 
 describe('Desktop Route Testing', function() {
     describe('importVolumeData()', function() {
-        
+        it('Should load it in correctly', function() {
+
+        })
     })
 
     describe('getPerformanceUsage', function() {
@@ -16,6 +19,62 @@ describe('Desktop Route Testing', function() {
     describe('getModuleSettings', function() {
 
     })
+})
+
+describe('File Utils', function() {
+})
+
+describe('Server Route Testing', function() {
+    var client
+
+    it('Should connect to the server in 3 seconds', function(done) {
+        var connected = false
+        var options ={
+            transports: ['websocket'],
+            'force new connection': true
+        };
+
+        client = io.connect('http://192.168.1.78:3000', options)
+        //client.open()
+        
+        client.on('connect', function(data) {
+            //client.emit('play', '123')
+
+            connected = true
+            expect(connected).to.be.equal(true)
+        })
+
+        client.on('connect_error', (error) => {
+            // Throw an error
+            console.log('Connect error: ' + error)
+
+            expect(client.connected).to.be.equal(true)
+        })
+
+        client.on('error', (error) => {
+            // Throw an error
+            console.log('General error: ' + error)
+
+            expect(client.connected).to.be.equal(true)
+        })
+
+        client.on('connect_timeout', (timeout) => {
+            // Throw an error
+            console.log('Timeout ' + timeout)
+
+            expect(client.connected).to.be.equal(true)
+        })
+        
+        this.timeout(5000)
+        setTimeout(function() {
+            // console.log(client)
+            expect(client.connected).to.be.equal(true)
+
+            done()
+        }, 3000)
+    })
+
+    
 })
 
 describe('addTwoNumbers', function() { // Creates a testing environment
