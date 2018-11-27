6a71bd7d9685acdbdc63e8a78dd610c83a632461
gannonprudhomme <gannonprudhomme@gmail.com>
2018-11-27 03:12:47 -0600

    Removed unneccessary helper function in windows testing

--- test/windows.js
+++ test/windows.js
@@ -6,7 +6,7 @@ var isWin = process.platform === "win32"
 
 if(isWin) {
 
-    
+
 var expect = require('chai').expect
 var assert = require('chai').assert
 var tasks = require('../routes/tasks.js')
@@ -17,32 +17,20 @@ describe('Tasks Testing', function() {
             
         })
     })
-
+        
     describe('Loading task list', function() {
         it('Should have System Process (on Windows)', async function() {
             return tasks.loadTaskList().then(taskMap => {
-                expect(isKeyInMap(taskMap, 'System')).to.be.equal(true)
+                expect(taskMap.has('System')).to.be.equal(true)
             })
         })
 
         it('Should have Registry (on Windows)', async function() {
             return tasks.loadTaskList().then(taskMap => {
-                expect(isKeyInMap(taskMap, 'Registry')).to.be.equal(true)
+                expect(taskMap.has('Registry')).to.be.equal(true)
             })
         })
     })
 })
-
-// Helper function for testing if a key is within a map's list of keys
-function isKeyInMap(map, key) {
-    for(var k of map.keys()) {
-        if(key === k) {
-            return true
-        }
-    }
-
-    return false
-}
-
 
 }
