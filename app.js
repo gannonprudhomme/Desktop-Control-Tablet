002d80dcae425fb93301284219138b2aa0a40911
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-02-17 00:43:12 -0600

    Readded spotify authentication to app.js

--- app.js
+++ app.js
@@ -23,6 +23,12 @@
   var fluxbulb = require('./routes/lights.js')  // controlling tp-link lightbulb from f.lux 
   var tasks = require('./routes/tasks.js')
 
+  var communication = require('./routes/communication.js')
+
+  app.set('views', path.join(__dirname + '/public/views'));
+  app.set('view engine', 'pug') // 'hbs' is connected to the app.engine('hbs', ...)
+  app.locals.baseDir = path.join(__dirname + '/public/views') // Set options.baseDir for Pug
+
   app.use(discord)
   app.use(socket)
   app.use(fluxbulb.router)
@@ -32,6 +38,44 @@
   app.use(bodyParser.json()); // For parsing application/json
   app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
+  var spotifyAuthenticated = false
+
+  app.get('/tablet', (req, res) => {
+    if(!spotifyAuthenticated) {
+      spotify.authenticateSpotify(res)
+
+      spotifyAuthenticated = true;
+    } else {
+      // Load in the settings file
+      var json = JSON.parse(fs.readFileSync(path.join(__dirname + '/view-settings.json'), 'utf8'))
+      var options = {
+        show_current_time: json['show-current-time'], 
+        quickIcons: json['quickIcons'],
+        modules: json['modules'],
+        currentModules: json['currentModules'],
+        volumeMixers: json['volume-mixers']
+      }
+      
+      // Load in all of the settings data for all of our current modules
+      var modSettings = desktop.getModuleSettings(json['currentModules'])
+
+      // Add the module settings to the above option settings, to be sent to Pug to be used in our page
+      options = {...options, ...modSettings}
+
+      // Render the webpage
+      res.render('index', options)
+          
+      // After authenticating, get the access and refresh tokens
+      // have a function that takes req as a parameter(or req.query)
+      var code = req.query.code || null
+      var state = req.query.code || null
+
+      // Get authentication tokens for use in Spotify
+      spotify.getAuthArguments(code, state)
+    }
+  })
+
+  
   // Listen to this port, and handle any errors accordingly
   server.listen(port, (err) => {
     if(err) {
