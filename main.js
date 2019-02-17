2ee7f55bb97ead9196b66a2ec59bcafa1309647b
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-02-17 00:44:11 -0600

    Changed main.js URL loading to node.js

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 const {app, BrowserWindow} = require('electron')
 const setupPug = require('electron-pug')
 
-var express = require('express')
-var multer = require('multer') // for parsing multipart/form-data
-var bodyParser = require('body-parser') // for parsing basic request data?
 var path = require('path')
 var fs = require('fs')
+var os = require('os')
 var queryString = require('querystring')
+var server = require('./app.js')
 
 var desktop = require('./routes/desktop.js')
+var spotify = require('./routes/spotify.js')
 
 let mainWindow
 
+var spotifyAuthenticated = false
+
+// Create the window
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
@@ -28,39 +31,12 @@ function createWindow() {
     console.log('created window')
 }
 
+// When electron is ready, load the pug file and its settings and render it
 app.on('ready', async() => {
-    var json = JSON.parse(fs.readFileSync(path.join(__dirname + '/view-settings.json'), 'utf8'))
-        var options = {
-            show_current_time: json['show-current-time'], 
-            quickIcons: json['quickIcons'],
-            modules: json['modules'],
-            currentModules: json['currentModules'],
-            volumeMixers: json['volume-mixers']
-        }
-        
-        // Load in all of the settings data for all of our current modules
-        var modSettings = desktop.getModuleSettings(json['currentModules'])
-        
-        // Add the module settings to the above option settings, to be sent to Pug to be used in our page
-        options = {...options, ...modSettings}
-        
-        try {
-            let pug = await setupPug({pretty: true}, options)
-            pug.on('error', err => console.error('electron-pug error', err))
-            console.log('setup pug')
-        } catch(err) {
-            console.err()
-        }
-        
-        // Render the webpage
-        //res.render('index', options)
-        
-        // Create window
-        createWindow()
-        
-        console.log('loading url')
+    // Create window
+    createWindow()
 
-        mainWindow.loadFile('./public/views/index.pug')
+    mainWindow.loadURL('http://localhost:3000/tablet')
 })
 
 
