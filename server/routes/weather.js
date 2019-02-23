001458b136a2ce3839ed6dcaba3606e1dd0fcd1d
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-02-23 17:34:33 -0600

    Made  weather module a Route subclass

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -1,46 +1,54 @@
-var weather = require('weather-js')
-var fs = require('fs')
-var weatherSettings = JSON.parse(fs.readFileSync('./public/views/modules/weather.json'))
+const weather = require('weather-js')
+const fs = require('fs')
+const Route = require('./route.js')
 
-var socketHandler = function(socket) {
+class Weather extends Route {
+  constructor() {
+    super()
+
+    this.weatherSettings = JSON.parse(fs.readFileSync('./public/views/modules/weather.json'))
+  }
+
+  socketHandler(socket) {
     socket.on('get_forecast', function(data, ret) {
-        getForecast().then((data) => {
-            ret(data)
-        }).catch((error) => {
-            console.log(error)
-        })
+      getForecast().then((data) => {
+        ret(data)
+      }).catch((error) => {
+        console.log(error)
+      })
     })
-}
+  }
 
-function getForecast() {
+  // Retrieve the current forecast and return it as a dictionary
+  getForecast() {
     return new Promise((result, reject) => {
-        weather.find({search: weatherSettings['zipcode'], degreeType: 'F'}, function(error, d) {
-            if(error) { // If there was an error
-                console.log(error)
-                reject(error)
-            }
-            
-            var retData = {}
+      weather.find({search: weatherSettings['zipcode'], degreeType: 'F'}, function(error, d) {
+        if(error) { // If there was an error
+          console.log(error)
+          reject(error)
+        }
 
-            // If the data was loaded
-            if(d) {
-                var data = d[0]
+        const retData = {}
 
-                retData['currentTemp'] = data['current']['temperature']
-                retData['currentWeather'] = data['current']['skytext']
-                retData['futureForecasts'] = data['forecast'] // Forecasts for the today + next 3 days
-                result(retData)
+        // If the data was loaded
+        if(d) {
+          const data = d[0]
 
-            } else { // Couldn't load the data, return invalid data
-                console.log('Weather data couldnt be loaded!')
-                retData['currentTemp'] = -99
-                retData['currentWeather'] = 'N/A'
-                retData['futureForecasts'] = [] // Forecasts for the today + next 3 days
-                result(retData)
-
-            }
-        })
+          retData['currentTemp'] = data['current']['temperature']
+          retData['currentWeather'] = data['current']['skytext']
+          retData['futureForecasts'] = data['forecast'] // Forecasts for the today + next 3 days
+          result(retData)
+        } else { // Couldn't load the data, return invalid data
+          console.log('Weather data couldnt be loaded!')
+          retData['currentTemp'] = -99
+          retData['currentWeather'] = 'N/A'
+          retData['futureForecasts'] = [] // Forecasts for the today + next 3 days
+          result(retData)
+        }
+      })
     })
+  }
 }
 
-module.exports.socketHandler = socketHandler
+
+module.exports = Weather
