48509b6a467ed4136f70a5277ff4762cb0556feb
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-02-24 01:20:40 -0600

    Fixed desktop.js errors

--- server/routes/desktop.js
+++ server/routes/desktop.js
@@ -3,16 +3,18 @@ const FileUtils = require('../fileutils.js')
 const Route = require('./route.js')
 
 class Desktop extends Route {
-  constructor() {
+  constructor(settings) {
     super()
 
     // Turn this into a function?
+    this.volumes = {}
     this.volumeData = JSON.parse(fs.readFileSync('./public/volumeData.json', 'utf-8'))
     this.volumeMixerData = JSON.parse(fs.readFileSync('./public/views/modules/volume-mixer.json', 'utf-8'))
-    this.volumes = {}
 
-    this.settingsData = JSON.parse(fs.readFileSync('./view-settings.json', 'utf-8'))
-    this.currentAudioDevice = settingsData['audioDevices'][0]
+    this.settingsData = settings
+    this.currentAudioDevice = this.settingsData['audioDevices'][0]
+
+    this.fileUtils = new FileUtils()
 
     this.moduleSettings = {}
   }
@@ -22,43 +24,29 @@ class Desktop extends Route {
       // console.log('user disconnected desktop')
     })
 
-
     socket.on('browser_error', function(data) {
       console.log(data)
     })
 
     // Send the settings back to the client
-    socket.on('settings', function(data, ret) {
+    socket.on('settings', (data, ret) => {
       console.log('Sending settings')
 
+      const modSettings = this.getModuleSettings(this.settingsData['currentModules'])
 
-      settingsData = JSON.parse(fs.readFileSync('./view-settings.json'), 'utf-8')
+      // TODO: Give this a better name
+      const bothSettings = {...this.settingsData, ...modSettings}
 
-      const modSettings = getModuleSettings(settingsData['currentModules'])
-
-      settingsData = {...settingsData, ...modSettings}
-
-      ret(settingsData)
-    })
-
-    // Is this unused?
-    socket.on('set_settings', function(data) {
-      console.log(settings)
-
-      console.log('Updated settings with: ')
-      for(let i = 0; i < settings.length; i++) {
-        if(settings[i] !== data[i]) {
-          console.log(data[i])
-        }
-      }
+      ret(bothSettings)
     })
 
     // Retrieve the module settings to be used in creating the HTML elements in settings
     // Returns it as a (module.id, module json settings) pair
-    socket.on('module_settings', function(data, ret) {
+    socket.on('module_settings', (data, ret) => {
       let moduleSettings;
 
       const currentModules = settingsData['currentModules']
+
       for(const mod in currentModules) {
         if(Object.prototype.hasOwnProperty.call(currentModules, mod)) {
           const modData = currentModules[mod] // The module data from view-settings.json
@@ -87,26 +75,26 @@ class Desktop extends Route {
       ret(json)
     })
 
-    socket.on('volume-mixer-settings', function(data, ret) {
+    socket.on('volume-mixer-settings', (data, ret) => {
       console.log('Sending volume-mixer-settings')
-      ret(volumeMixerData)
+      ret(this.volumeMixerData)
     })
 
-    socket.on('volume_data', function(data, ret) {
+    socket.on('volume_data', (data, ret) => {
       console.log('Sending volume Data!')
-      ret(volumes)
+      ret(this.volumes)
     })
   }
 
   importVolumeData() {
     // Fill the volume map with all of the previous data here
-    volumeData = FileUtils.loadVolumeData()
+    this.volumeData = FileUtils.loadVolumeData()
 
     // Iterate over all of the keys(programs) in the json object
     // And add them to the local map
-    for(const key in volumeData) {
-      if(Object.prototype.hasOwnProperty.call(volumeData, key)) {
-        volumes[key] = volumeData[key];
+    for(const key in this.volumeData) {
+      if(Object.prototype.hasOwnProperty.call(this.volumeData, key)) {
+        this.volumes[key] = this.volumeData[key];
       }
     }
   }
@@ -131,13 +119,13 @@ class Desktop extends Route {
       }
     }
 
-    return moduleSettings
+    return this.moduleSettings
   }
 
   setVolume(program, volume) {
-    volumes[currentAudioDevice][program] = volume
+    this.volumes[this.currentAudioDevice][program] = volume
 
-    fileUtils.saveVolumeData(volumes)
+    FileUtils.saveVolumeData(this.volumes)
   }
 
   setCurrentAudioDevice(device) {
