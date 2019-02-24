37228027c3fe48255adcf65c9395ac09b4c144cd
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-02-23 23:47:27 -0600

    Made socket.js into an ES6 class

--- server/routes/sockets.js
+++ server/routes/sockets.js
@@ -1,24 +1,24 @@
 // Joins all of the socket connections into one
+const express = require('express')
+const router = express.Router()
 
-var handleDesktop = require('./desktop.js')
-var handleSpotify = require('./spotify.js')
-var handleLight = require('./lights.js')
-var handleWeather = require('./weather.js')
-var handleCommunication = require('../communication.js')
-var express = require('express')
-var router = express.Router()
+// Handles all of the socket endpoints
+class SocketHandler {
+  constructor(routers) {
+    this.routers = routers
+  }
 
-var returnRouter = function(io) {
-  io.on('connection', function(socket) {
-    console.log('connection!')
-    handleDesktop.socketHandler(socket)
-    handleSpotify.socketHandler(socket)
-    handleLight.socketHandler(socket)
-    handleWeather.socketHandler(socket)
-    handleCommunication.socketHandler(socket)
-  })
+  returnRouter(io) {
+    io.on('connection', (socket) => {
+      // Call all of the routers' socketHandler functions
+      for(let i = 0; i < this.routers.length; i++) {
+        const route = this.routers[i]
 
-  return router
+        // TODO: Do i need to add a vailidity check here?
+        route.socketHandler(i)
+      }
+    })
+  }
 }
 
-module.exports = returnRouter
+module.exports = SocketHandler
