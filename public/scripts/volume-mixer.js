ee278bb66d8c95dd743a1a19ac1ca1b4717f9fdf
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-02-24 13:13:14 -0600

    Updated volume-mixer.js to ES6

--- public/scripts/volume-mixer.js
+++ public/scripts/volume-mixer.js
@@ -1,14 +1,13 @@
 // var volumes = {"master-volume":0, "spotify.exe": 0, "discord.exe": 0, "rocketleague.exe": 0, "chrome.exe": 0}
-var volumeData = {}
-var socket = require('socket.io-client')('http://localhost:3000')
+let volumeData = {}
 
-var settings = {}
-var currentDeviceName = "DAC"
-var currentDeviceIndex = 0
-var audioDevices = []
+// var settings = {}
+let currentDeviceName = "DAC"
+let currentDeviceIndex = 0
+let audioDevices = []
 
 // If we've initialized everything or not
-var isInitialized = false
+let isInitialized = false
 
 $(document).ready(function() {
   // On launch, load the settings file
@@ -39,7 +38,6 @@ function initialize() {
 
   window.setInterval(function() {
     getActivePrograms()
-  
   }, 3000)
 
   isInitialized = true
@@ -47,51 +45,51 @@ function initialize() {
 }
 
 // Prevents weird/unnatural sliding of the document
-$(document.body).on("touchmove", function(event) {
-    event.preventDefault();
-    event.stopPropagation();
+$(document.body).on('touchmove', function(event) {
+  event.preventDefault();
+  event.stopPropagation();
 });
 
 $('#audio-device').click(function() {
   currentDeviceIndex = (currentDeviceIndex + 1) % audioDevices.length
   currentDeviceName = audioDevices[currentDeviceIndex]
 
-  console.log(currentDeviceIndex + " " + currentDeviceName)
+  console.log(currentDeviceIndex + ' ' + currentDeviceName)
 
   socket.emit('audio_device', currentDeviceName)
 
-  //console.log(volumeData[currentDeviceName])
+  // console.log(volumeData[currentDeviceName])
 
   changeSliderValues(volumeData[currentDeviceName])
 })
 
 // Dynamically create(init) the sliders, connecting them to their respective HTML id's
 function createSlider(id, programName) {
-  //console.log('Creating slider ' + id + ' ' + programName)
+  // console.log('Creating slider ' + id + ' ' + programName)
   // Generate the jQuery UI slider, at the appropriate ID
   $('#' + id + '-slider').slider({
     value: volumeData[currentDeviceName][programName] * 100,
     min: 0,
     max: 100,
-    animate: "fast",
-    orientation: "horizontal",
+    animate: 'fast',
+    orientation: 'horizontal',
 
     slide: function(e, ui) {
-      $('#' + id + '-label').text(ui.value + "%")
+      $('#' + id + '-label').text(ui.value + '%')
       sendVolumeData(programName, ui.value / 100)
       volumeData[currentDeviceName][programName] = ui.value / 100
-    }
+    },
   })
 
-  $('#' + id + '-label').text(Math.floor(volumeData[currentDeviceName][programName] * 100) + "%")
+  $('#' + id + '-label').text(Math.floor(volumeData[currentDeviceName][programName] * 100) + '%')
 }
 
 // Loop and create all of the volume sliders
 function setSliders() {
-  //console.log(settings)
-  var volume_mixers = settings['volumeMixers']
-  for(var i = 0; i < volume_mixers.length; i++) {
-    var mixer = volume_mixers[i];
+  // console.log(settings)
+  const volumeMixers = settings['volumeMixers']
+  for(const i = 0; i < volumeMixers.length; i++) {
+    const mixer = volume_mixers[i];
 
     createSlider(mixer['id'], mixer['programName'])
   }
@@ -99,43 +97,44 @@ function setSliders() {
 
 // Change the slider values when swapping audio outputs
 function changeSliderValues() {
-  //console.log(settings)
-  var volume_mixers = settings['volumeMixers']
-  for(var i = 0; i < volume_mixers.length; i++) {
-    var mixer = volume_mixers[i];
+  // console.log(settings)
+  const volumeMixers = settings['volumeMixers']
+  for(const i = 0; i < volumeMixers.length; i++) {
+    const mixer = volumeMixers[i];
 
     $('#' + mixer['id'] + '-slider').slider('value', volumeData[currentDeviceName][mixer['programName']] * 100)
-    $('#' + mixer['id'] + '-label').text(parseInt(volumeData[currentDeviceName][mixer['programName']] * 100) + "%")
+    $('#' + mixer['id'] + '-label').text(parseInt(volumeData[currentDeviceName][mixer['programName']] * 100) + '%')
   }
 }
 
 // Send volume data back to the server/
 function sendVolumeData(program, volume) {
-  var obj = {
+  const obj = {
     program: program,
     volume: volume,
-    time: (new Date()).getTime()
+    time: (new Date()).getTime(),
   }
 
-  //console.log('sending volume')
+  // console.log('sending volume')
   socket.emit('set_volume', obj)
 }
 
 function getVolumeData() {
-  //console.log('Volume: Attempting to retrieve volume data')
+  // console.log('Volume: Attempting to retrieve volume data')
   socket.emit('volume_data', '', function(data) {
-    var i = 0
+    const i = 0
     console.log('Retrieved volume data')
-    
-    for(var key in data) {
-      volumeData[key] = data[key];
-      
-      audioDevices[i] = key
-      
-      i++
+
+    for(const key in data) {
+      if(Object.prototype.hasOwnProperty.call(data, key)) {
+        volumeData[key] = data[key];
+        audioDevices[i] = key
+
+        i++
+      }
     }
 
-    console.log(volumeData)    
+    console.log(volumeData)
     currentDeviceName = audioDevices[currentDeviceIndex]
 
     // After the volume data is loaded, initialize the sliders
@@ -147,7 +146,7 @@ function getVolumeData() {
 function getActivePrograms() {
   // Attempt to retrieve the list of active programs
   socket.emit('active_programs', '', function(data) {
-    for(var id in data) {
+    for(const id in data) {
       // If the program is active
       if(data[id]) {
         // Display it
