8d244a06b988101fa4f12ce0632909d7b3275a13
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-02-25 01:33:39 -0600

    Updated light-control test file

--- test/light-controlspec.js
+++ test/light-controlspec.js
@@ -1,12 +1,12 @@
-var expect = require('chai').expect
-var assert = require('chai').assert
-var fs = require('fs')
-var io = require('socket.io-client')
+const expect = require('chai').expect
+const assert = require('chai').assert
+const fs = require('fs')
+const io = require('socket.io-client')
 
 describe('Light Control Module', function() {
-    describe('Socket Endpoints', function() {
-        it('Should connect', function() {
-            
-        })
+  describe('Socket Endpoints', function() {
+    it('Should connect', function() {
+
     })
+  })
 })
