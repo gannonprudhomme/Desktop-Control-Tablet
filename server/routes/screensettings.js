15a0a535bb96629a585ce01fbfd0742ec23521ca
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-02-26 18:31:59 -0600

    Added promise handling to screensettings rpi_get_info route

--- server/routes/screensettings.js
+++ server/routes/screensettings.js
@@ -11,13 +11,31 @@ class ScreenSettings extends Route {
 
   socketHandler(socket) {
     socket.on('rpi_get_info', (data, ret) => {
+      // TODO: Need to return something if there is an error
       const retData = {}
-      retData['minBrightness'] = 9 // Anything below 9 isn't visible
-      retData['maxBrightness'] = backLight.getMaxBrightness()
-      retData['brightness'] = backLight.getBrightness()
-      retData['power'] = backLight.isPoweredOn()
 
-      ret(retData)
+      // Retrieve all of the rpi screen information
+      backLight.getMaxBrightness().then((maxBrightness) => {
+        retData['minBrightness'] = 9
+        retData['maxBrightness'] = maxBrightness
+
+        return backLight.getBrightness()
+      }).catch((err) => {
+        console.log(err)
+      }).then((brightness) => {
+        retData['brightness'] = brightness
+
+        return backLight.isPoweredOn()
+      }).catch((err) => {
+        console.log(err)
+      }).then((isPowered) => {
+        retData['power'] = isPowered
+
+        console.log('ScreenSettings: Returning rpi data!')
+        ret(retData)
+      }).catch((err) => {
+        console.log(err)
+      })
     })
 
     socket.on('rpi_set_brightness', (data) => {
