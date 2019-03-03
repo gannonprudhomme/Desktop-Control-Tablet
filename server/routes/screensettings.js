0e70cb579bf0365a3d6cee27afcaf8f94640f090
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-03-02 22:24:05 -0600

    Added platform check to screensettings route

--- server/routes/screensettings.js
+++ server/routes/screensettings.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 const Route = require('./route.js')
 
-// Need to add a check to see if the client is running on a Raspberry Pi
-const backLight = require('rpi-backlight')
+let backLight = '';
+if(isPi()) {
+  // Need to add a check to see if the client is running on a Raspberry Pi
+  backLight = require('rpi-backlight')
+}
 
 class ScreenSettings extends Route {
   constructor() {
@@ -13,44 +16,63 @@ class ScreenSettings extends Route {
     socket.on('rpi_get_info', (data, ret) => {
       // TODO: Need to return something if there is an error
       const retData = {}
+      if(isPi()) {
+        // Retrieve all of the rpi screen information
+        backLight.getMaxBrightness().then((maxBrightness) => {
+          retData['minBrightness'] = 9
+          retData['maxBrightness'] = maxBrightness
 
-      // Retrieve all of the rpi screen information
-      backLight.getMaxBrightness().then((maxBrightness) => {
-        retData['minBrightness'] = 9
-        retData['maxBrightness'] = maxBrightness
+          return backLight.getBrightness()
+        }).catch((err) => {
+          console.log(err)
+        }).then((brightness) => {
+          retData['brightness'] = brightness
 
-        return backLight.getBrightness()
-      }).catch((err) => {
-        console.log(err)
-      }).then((brightness) => {
-        retData['brightness'] = brightness
+          return backLight.isPoweredOn()
+        }).catch((err) => {
+          console.log(err)
+        }).then((isPowered) => {
+          retData['power'] = isPowered
 
-        return backLight.isPoweredOn()
-      }).catch((err) => {
-        console.log(err)
-      }).then((isPowered) => {
-        retData['power'] = isPowered
+          console.log('ScreenSettings: Returning rpi data!')
+          ret(retData)
+        }).catch((err) => {
+          console.log(err)
+        })
+      } else {
+        const retData = {
+          brightness: 0,
+          power: false,
+          minBrightness: 8, // The screen isn't visible anything past 9%
+          maxBrightness: 100,
+        }
 
-        console.log('ScreenSettings: Returning rpi data!')
         ret(retData)
-      }).catch((err) => {
-        console.log(err)
-      })
+      }
     })
 
     socket.on('rpi_set_brightness', (data) => {
-      backLight.setBrightness(data)
+      if(isPi()) {
+        backLight.setBrightness(data)
+      }
     })
 
     // Turn the raspberry pi screen on/off
     socket.on('rpi_set_screen_power', (data) => {
-      if(data) {
-        backLight.powerOn()
-      } else {
-        backLight.powerOff()
+      if(isPi()) {
+        if(data) {
+          backLight.powerOn()
+        } else {
+          backLight.powerOff()
+        }
       }
     })
   }
+}
+
+function isPi() {
+  // For now, people probably won't be building on Linux, so we can assume it's the Pi
+  return process.platform === 'linux'
 }
 
 module.exports = ScreenSettings
