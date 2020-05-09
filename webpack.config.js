e895b1722abfe0c1d32a0e49ebefccf17eafd5f4
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-05-08 19:02:37 -0500

    Added typescript & webpack config

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  mode: 'production',
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.css'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        }
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: '@teamsupercell/typings-for-css-modules-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              localsConvention: 'camelCase',
+              modules: {
+                mode: 'local',
+                localIdentName: '[local]--[has:base64:5]',
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  // When importing a module whose path matches one of the following, just assume a corresponding
+  // global variable exists and use that instead. This is important b/c it allows us to avoid
+  // bundling all of our dependencies, which allows browsers to cache those libraries between builds
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
+}
