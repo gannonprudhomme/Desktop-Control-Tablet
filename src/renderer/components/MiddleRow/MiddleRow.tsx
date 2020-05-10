c0d98085044d2f09153ad65262fcff017325f520
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-05-10 17:02:56 -0500

    Added MiddleRow outline

---
+++ src/renderer/components/MiddleRow/MiddleRow.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as styles from './MiddleRow.css';
+
+const MiddleRow: React.FC = () => {
+  return (
+    <div id={styles.middleRow}>
+      MiddleRow
+    </div>
+  );
+};
+
+export default MiddleRow;
