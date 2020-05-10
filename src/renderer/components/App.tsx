652c184124533affa43608aa7d66ce46e17ed885
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-05-10 18:14:53 -0500

    Added song retrieving & styles to App

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 
 import TopRow from './TopRow/TopRow';
 import MiddleRow from './MiddleRow/MiddleRow';
 import BottomRow from './BottomRow/BottomRow';
 import * as styles from './App.css';
+import { spotifyController } from '../framework/SocketHandler';
+import changeCurrentlyPlaying from '../redux/actions/currentlyPlaying';
 
 const App: React.FC = () => {
-  const thing = 5;
+  const dispatch = useDispatch();
+
+  spotifyController.getSong().then(
+    (song) => dispatch(changeCurrentlyPlaying(song)),
+  );
+
+  setInterval(() => {
+    // Attempt to get the current song
+    spotifyController.getSong().then(
+      (song) => dispatch(changeCurrentlyPlaying(song)),
+    );
+  }, 500);
 
   return (
     <div className={styles.gridContainer}>
