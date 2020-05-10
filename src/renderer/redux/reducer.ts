2806f99107d2086803bc8a1113496736d12446e1
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-05-09 21:58:29 -0500

    Added currentlyPlaying redux

--- src/renderer/redux/reducer.ts
+++ src/renderer/redux/reducer.ts
@@ -1,1 +1,10 @@
 // The main/root reducer for the project
+import { combineReducers } from 'redux';
+import currentlyPlaying from './reducers/currentlyPlaying';
+
+const dctReducer = combineReducers({
+  currentlyPlaying,
+});
+
+export default dctReducer;
+export type RootState = ReturnType<typeof dctReducer>;
