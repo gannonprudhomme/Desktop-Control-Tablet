aad4c35f946f90ec97c788aaf064b0340e166d12
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-05-10 17:01:02 -0500

    Updated App tests to use Redux store

--- src/renderer/tests/ui/App.test.tsx
+++ src/renderer/tests/ui/App.test.tsx
@@ -1,13 +1,22 @@
-import { act, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import * as React from 'react';
+import { Provider } from 'react-redux';
 
+import { createStore } from 'redux';
 import App from '../../components/App';
+import dctReducer from '../../redux/reducer';
 
 describe('App', () => {
   test('renders without errors', () => {
     // arrange / act
-    const container = render(<App />);
+    const store = createStore(dctReducer);
+
+    const container = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
 
     // assert
     expect(container).toBeTruthy();
