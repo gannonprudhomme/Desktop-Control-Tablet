4e602ecef1ad7384d1f6226592d4b45063490919
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-06-03 00:05:22 -0500

    Made MiddleRow show the current module

--- src/renderer/components/MiddleRow/MiddleRow.tsx
+++ src/renderer/components/MiddleRow/MiddleRow.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 import * as styles from './MiddleRow.css';
+import { RootState } from '../../redux/reducer';
+import ModuleArray from '../../types/Module';
 
 const MiddleRow: React.FC = () => {
+  const { modulesArray, currentModule } = useSelector<RootState, ModuleArray>(
+    (state) => state.modules,
+  );
+
+  // The currently displayed module
+  const [currentView, setCurrentView] = React.useState(null);
+
+  React.useEffect(() => {
+    if (currentModule) {
+      const currComp = <currentModule.component key={currentModule.name} />;
+
+      // Not sure why we have to resolve this but whatever
+      Promise.resolve(currComp).then(setCurrentView);
+    }
+  }, [modulesArray, currentModule]);
+
   return (
     <div id={styles.middleRow}>
-      MiddleRow
+      {/* TODO: Maybe add a better no modules thing? */}
+      {currentView ?? 'No modules enabled!'}
     </div>
   );
 };
---
+++ src/renderer/tests/ui/MiddleRow.test.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { createStore } from 'redux';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import dctReducer from '../../redux/reducer';
+import { addModule, changeCurrentModule } from '../../redux/actions/modules';
+import MiddleRow from '../../components/MiddleRow/MiddleRow';
+import Empty from '../utils/Empty';
+
+describe('MiddleRow UI', () => {
+  describe('changes the currently displaying module', () => {
+    test('when the currentModule state changes', async () => {
+      // arrange
+      const module1Comp: React.FC = () => (<div> Module1 </div>);
+      const module2Comp: React.FC = () => (<div> Module2 </div>);
+
+      const store = createStore(dctReducer);
+      const module1 = {
+        name: 'Module1',
+        component: module1Comp,
+        icon: null as string,
+        reducer: null as Function,
+      };
+      const module2 = {
+        ...module1,
+        name: 'Module2',
+        component: module2Comp,
+      };
+      store.dispatch(addModule(module1));
+      store.dispatch(addModule(module2));
+      store.dispatch(changeCurrentModule(module1));
+
+      const { findByText } = render(
+        <Provider store={store}>
+          <MiddleRow />
+        </Provider>,
+      );
+
+      // act
+      await findByText('Module1'); // First ensure that Module1 text is there
+
+      // then actually change the module
+      store.dispatch(changeCurrentModule(module2));
+
+      // assert
+      const module2Text = await findByText('Module2');
+      expect(module2Text).toBeInTheDocument();
+    });
+  });
+});
