78c5b1db15c39e4ac9944ccdf337a57d09154ff9
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-06-03 02:30:00 -0500

    Removed commands file

--- server/commands.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const stream = fs.createWriteStream('delays.txt', {flags: 'a'})
-
-function saveDelay(time) {
-  stream.write(time + '\n')
-}
-
-module.exports.saveDelay = saveDelay
