6a3a6d9bcae39795ed12af87b6e1151152e60e68
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-11-16 16:18:13 -0600

    Made server/icons folder created in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 echo "Creating Volume Mixer settings from template"
 # -n flag prevents it from overwriting existing files
 cp -n ./src/renderer/modules/VolumeMixer/settings-template.json ./src/renderer/modules/VolumeMixer/settings.json
+
+echo "Making the ./server/icons folder"
+mkdir ./server/icons
